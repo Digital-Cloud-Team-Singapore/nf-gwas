6694f1297f3e57a488a8afba8298cda097036349
minhtoo.lin <linmin.htoo@gmail.com>
2024-02-09 12:41:45 +0800

    feat(pheweb): add example files

---
+++ pheweb/example_config.py
@@ -0,0 +1,9 @@
+hg_build_number = 19
+# change the cache value to an appropriate one to be shared by all instances of pheweb
+cache = "/Users/minhtoo.lin/repos/nf-gwas/healthomics/pheweb/cache/"
+pval_is_neglog10 = True
+# column names are case-insensitive
+field_aliases = {
+    "GENPOS": "pos",
+    "LOG10P": "pval"
+}
