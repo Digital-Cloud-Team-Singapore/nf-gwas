0b3d4aaf91c11a028f830b4d14f2df3dd9565ca7
minhtoo.lin <linmin.htoo@gmail.com>
2024-02-15 14:32:59 +0800

    chore: add some initial cost estimates

--- healthomics/python_helpers/draw_timeline_example.sh
+++ healthomics/python_helpers/draw_timeline_example.sh
@@ -3,4 +3,8 @@
 # -u is time units, sec or min or hr or day
 # 1 million variants x 50k samples, quantitative phenotype
 # 33 cents, 125 mins
-python3 timeline.py 2646381 --region ap-southeast-1 -u min
+python3 timeline.py 4414369 --region ap-southeast-1 -u min
+
+# $1.09 for chinese. 100 phenotypes. 60k samples. 100k variants total. 21 chromosomes.
+# 88 million variants --> $1.09 x 880 = $959.20
+# scaling of price per phenotype is uncertain. it's sub-linear (N phenotypes doesn't mean Nx price, but it's less than Nx)
